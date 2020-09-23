04c900534b2fe6bb486c0bd89d4b1671ce4e9cf2
Saar <notnow@maybelater.com>
2020-09-23 20:27:59 +0300

    Created ObjectProperty and SimpleObjectProperty

--- src/main/java/org/jproperty/type/ObjectProperty.java
+++ src/main/java/org/jproperty/type/ObjectProperty.java
@@ -1,70 +1,7 @@
 package org.jproperty.type;
 
-import org.jproperty.*;
-import org.jproperty.binding.Bindings;
+import org.jproperty.Property;
 
-public class ObjectProperty<T> implements ReadOnlyObjectProperty<T>, Property<T> {
+public interface ObjectProperty<T> extends ReadOnlyObjectProperty<T>, Property<T> {
 
-    private ReadOnlyProperty<? extends T> bound = null;
-
-    protected ListenersHelper<T> helper = ListenersHelper.empty();
-
-    protected T value;
-
-    public ObjectProperty() {
-        this.value = null;
-    }
-
-    public ObjectProperty(T value) {
-        this.value = value;
-    }
-
-    @Override
-    public void addListener(ChangeListener<? super T> listener) {
-        this.helper = this.helper.addListener(listener);
-    }
-
-    @Override
-    public void removeListener(ChangeListener<? super T> listener) {
-        this.helper = this.helper.removeListener(listener);
-    }
-
-    @Override
-    public T getValue() {
-        return this.value;
-    }
-
-    @Override
-    public void bind(ReadOnlyProperty<? extends T> observable) {
-        Bindings.bind(this, observable);
-        this.bound = observable;
-    }
-
-    @Override
-    public void unbind() {
-        if (this.bound != null) {
-            Bindings.unbind(this, this.bound);
-            this.bound = null;
-        }
-    }
-
-    @Override
-    public void bindBidirectional(Property<T> observable) {
-        Bindings.bindBidirectional(this, observable);
-    }
-
-    @Override
-    public void unbindBidirectional(Property<T> observable) {
-        Bindings.unbindBidirectional(this, observable);
-    }
-
-    @Override
-    public void setValue(T value) {
-        if (this.value != value) {
-            final ChangeEventBase<T> event = new ChangeEventBase<>(this, this.value, value);
-
-            this.value = value;
-            this.helper.fireEvent(event);
-        }
-    }
 }
---
+++ src/main/java/org/jproperty/type/SimpleObjectProperty.java
@@ -0,0 +1,70 @@
+package org.jproperty.type;
+
+import org.jproperty.*;
+import org.jproperty.binding.Bindings;
+
+public class SimpleObjectProperty<T> implements ObjectProperty<T> {
+
+    private ReadOnlyProperty<? extends T> bound = null;
+
+    protected ListenersHelper<T> helper = ListenersHelper.empty();
+
+    protected T value;
+
+    public SimpleObjectProperty() {
+        this.value = null;
+    }
+
+    public SimpleObjectProperty(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public void addListener(ChangeListener<? super T> listener) {
+        this.helper = this.helper.addListener(listener);
+    }
+
+    @Override
+    public void removeListener(ChangeListener<? super T> listener) {
+        this.helper = this.helper.removeListener(listener);
+    }
+
+    @Override
+    public T getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void bind(ReadOnlyProperty<? extends T> observable) {
+        Bindings.bind(this, observable);
+        this.bound = observable;
+    }
+
+    @Override
+    public void unbind() {
+        if (this.bound != null) {
+            Bindings.unbind(this, this.bound);
+            this.bound = null;
+        }
+    }
+
+    @Override
+    public void bindBidirectional(Property<T> observable) {
+        Bindings.bindBidirectional(this, observable);
+    }
+
+    @Override
+    public void unbindBidirectional(Property<T> observable) {
+        Bindings.unbindBidirectional(this, observable);
+    }
+
+    @Override
+    public void setValue(T value) {
+        if (this.value != value) {
+            final ChangeEventBase<T> event = new ChangeEventBase<>(this, this.value, value);
+
+            this.value = value;
+            this.helper.fireEvent(event);
+        }
+    }
+}
