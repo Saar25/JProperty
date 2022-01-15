329b885ec8eaafbbb88b512ed828e662b785b84e
Saar Tako <saartako25@gmail.com>
2022-01-15 19:13:39 +0200

    added more bindings

--- src/main/java/org/jproperty/Extensions.kt
+++ src/main/java/org/jproperty/Extensions.kt
@@ -1,46 +1,47 @@
 package org.jproperty
 
-import org.jproperty.expression.*
+import org.jproperty.binding.Bindings
+import org.jproperty.binding.FloatBinding
+import org.jproperty.binding.IntegerBinding
 import org.jproperty.value.ObservableFloatValue
 import org.jproperty.value.ObservableIntegerValue
-import org.jproperty.value.ObservableNumberValue
 
-operator fun ObservableIntegerValue.plus(other: ObservableNumberValue): IntExpression = IntExpressions.add(this, other)
-operator fun ObservableIntegerValue.plus(other: Int): IntExpression = IntExpressions.add(this, other)
+operator fun ObservableIntegerValue.plus(other: ObservableIntegerValue): IntegerBinding = Bindings.add(this, other)
+operator fun ObservableIntegerValue.plus(other: Int): IntegerBinding = Bindings.add(this, other)
 
-operator fun ObservableIntegerValue.minus(other: ObservableNumberValue): IntExpression = IntExpressions.sub(this, other)
-operator fun ObservableIntegerValue.minus(other: Int): IntExpression = IntExpressions.sub(this, other)
+operator fun ObservableIntegerValue.minus(other: ObservableIntegerValue): IntegerBinding = Bindings.sub(this, other)
+operator fun ObservableIntegerValue.minus(other: Int): IntegerBinding = Bindings.sub(this, other)
 
-operator fun ObservableIntegerValue.times(other: ObservableNumberValue): IntExpression = IntExpressions.mul(this, other)
-operator fun ObservableIntegerValue.times(other: Int): IntExpression = IntExpressions.mul(this, other)
+operator fun ObservableIntegerValue.times(other: ObservableIntegerValue): IntegerBinding = Bindings.mul(this, other)
+operator fun ObservableIntegerValue.times(other: Int): IntegerBinding = Bindings.mul(this, other)
 
-operator fun ObservableIntegerValue.div(other: ObservableNumberValue): IntExpression = IntExpressions.div(this, other)
-operator fun ObservableIntegerValue.div(other: Int): IntExpression = IntExpressions.div(this, other)
+operator fun ObservableIntegerValue.div(other: ObservableIntegerValue): IntegerBinding = Bindings.div(this, other)
+operator fun ObservableIntegerValue.div(other: Int): IntegerBinding = Bindings.div(this, other)
 
-infix fun ObservableIntegerValue.max(other: ObservableNumberValue): IntExpression = IntExpressions.max(this, other)
-infix fun ObservableIntegerValue.max(other: Int): IntExpression = IntExpressions.max(other, this)
+infix fun ObservableIntegerValue.max(other: ObservableIntegerValue): IntegerBinding = Bindings.max(this, other)
+infix fun ObservableIntegerValue.max(other: Int): IntegerBinding = Bindings.max(other, this)
 
-infix fun ObservableIntegerValue.min(other: ObservableNumberValue): IntExpression = IntExpressions.min(this, other)
-infix fun ObservableIntegerValue.min(other: Int): IntExpression = IntExpressions.min(other, this)
+infix fun ObservableIntegerValue.min(other: ObservableIntegerValue): IntegerBinding = Bindings.min(this, other)
+infix fun ObservableIntegerValue.min(other: Int): IntegerBinding = Bindings.min(other, this)
 
-operator fun ObservableFloatValue.plus(other: ObservableNumberValue): FloatExpression = FloatExpressions.add(this, other)
-operator fun ObservableFloatValue.plus(other: Float): FloatExpression = FloatExpressions.add(this, other)
+operator fun ObservableFloatValue.plus(other: ObservableFloatValue): FloatBinding = Bindings.add(this, other)
+operator fun ObservableFloatValue.plus(other: Float): FloatBinding = Bindings.add(this, other)
 
-operator fun ObservableFloatValue.minus(other: ObservableNumberValue): FloatExpression = FloatExpressions.sub(this, other)
-operator fun ObservableFloatValue.minus(other: Float): FloatExpression = FloatExpressions.sub(this, other)
+operator fun ObservableFloatValue.minus(other: ObservableFloatValue): FloatBinding = Bindings.sub(this, other)
+operator fun ObservableFloatValue.minus(other: Float): FloatBinding = Bindings.sub(this, other)
 
-operator fun ObservableFloatValue.times(other: ObservableNumberValue): FloatExpression = FloatExpressions.mul(this, other)
-operator fun ObservableFloatValue.times(other: Float): FloatExpression = FloatExpressions.mul(this, other)
+operator fun ObservableFloatValue.times(other: ObservableFloatValue): FloatBinding = Bindings.mul(this, other)
+operator fun ObservableFloatValue.times(other: Float): FloatBinding = Bindings.mul(this, other)
 
-operator fun ObservableFloatValue.div(other: ObservableNumberValue): FloatExpression = FloatExpressions.div(this, other)
-operator fun ObservableFloatValue.div(other: Float): FloatExpression = FloatExpressions.div(this, other)
+operator fun ObservableFloatValue.div(other: ObservableFloatValue): FloatBinding = Bindings.div(this, other)
+operator fun ObservableFloatValue.div(other: Float): FloatBinding = Bindings.div(this, other)
 
-infix fun ObservableFloatValue.max(other: ObservableNumberValue): FloatExpression = FloatExpressions.max(this, other)
-infix fun ObservableFloatValue.max(other: Float): FloatExpression = FloatExpressions.max(other, this)
+infix fun ObservableFloatValue.max(other: ObservableFloatValue): FloatBinding = Bindings.max(this, other)
+infix fun ObservableFloatValue.max(other: Float): FloatBinding = Bindings.max(other, this)
 
-infix fun ObservableFloatValue.min(other: ObservableNumberValue): FloatExpression = FloatExpressions.min(this, other)
-infix fun ObservableFloatValue.min(other: Float): FloatExpression = FloatExpressions.min(other, this)
+infix fun ObservableFloatValue.min(other: ObservableFloatValue): FloatBinding = Bindings.min(this, other)
+infix fun ObservableFloatValue.min(other: Float): FloatBinding = Bindings.min(other, this)
 
-fun <T> ObservableValue<T>.mapToInt(mapper: (T) -> Int): IntExpression = Expressions.mapToInt(this, mapper)
-fun <T> ObservableValue<T>.mapToFloat(mapper: (T) -> Float): FloatExpression = Expressions.mapToFloat(this, mapper)
-fun <I, O> ObservableValue<I>.map(mapper: (I) -> O): Expression<O> = Expressions.map(this, mapper)
+fun <T> ObservableValue<T>.mapToInteger(mapper: (T) -> Int): IntegerBinding = Bindings.mapToInt(this, mapper)
+fun <T> ObservableValue<T>.mapToFloat(mapper: (T) -> Float): FloatBinding = Bindings.mapToFloat(this, mapper)
+fun <I, O> ObservableValue<I>.map(mapper: (I) -> O): Binding<O> = Bindings.map(this, mapper)
--- src/main/java/org/jproperty/binding/Bindings.java
+++ src/main/java/org/jproperty/binding/Bindings.java
@@ -1,7 +1,12 @@
 package org.jproperty.binding;
 
+import org.jproperty.ObservableValue;
+import org.jproperty.constant.ConstantFloatProperty;
+import org.jproperty.constant.ConstantIntegerProperty;
 import org.jproperty.value.ObservableFloatValue;
 import org.jproperty.value.ObservableIntegerValue;
+
+import java.util.function.Function;
 
 public final class Bindings {
 
@@ -19,6 +24,14 @@ public final class Bindings {
         };
     }
 
+    public static IntegerBinding add(ObservableIntegerValue a, int b) {
+        return add(a, new ConstantIntegerProperty(b));
+    }
+
+    public static IntegerBinding add(int a, ObservableIntegerValue b) {
+        return add(new ConstantIntegerProperty(a), b);
+    }
+
     public static IntegerBinding sub(ObservableIntegerValue a, ObservableIntegerValue b) {
         return new IntegerBinding(a, b) {
             @Override
@@ -26,6 +39,14 @@ public final class Bindings {
                 return a.get() + b.get();
             }
         };
+    }
+
+    public static IntegerBinding sub(ObservableIntegerValue a, int b) {
+        return sub(a, new ConstantIntegerProperty(b));
+    }
+
+    public static IntegerBinding sub(int a, ObservableIntegerValue b) {
+        return sub(new ConstantIntegerProperty(a), b);
     }
 
     public static IntegerBinding mul(ObservableIntegerValue a, ObservableIntegerValue b) {
@@ -37,6 +58,14 @@ public final class Bindings {
         };
     }
 
+    public static IntegerBinding mul(ObservableIntegerValue a, int b) {
+        return mul(a, new ConstantIntegerProperty(b));
+    }
+
+    public static IntegerBinding mul(int a, ObservableIntegerValue b) {
+        return mul(new ConstantIntegerProperty(a), b);
+    }
+
     public static IntegerBinding div(ObservableIntegerValue a, ObservableIntegerValue b) {
         return new IntegerBinding(a, b) {
             @Override
@@ -44,6 +73,14 @@ public final class Bindings {
                 return a.get() / b.get();
             }
         };
+    }
+
+    public static IntegerBinding div(ObservableIntegerValue a, int b) {
+        return div(a, new ConstantIntegerProperty(b));
+    }
+
+    public static IntegerBinding div(int a, ObservableIntegerValue b) {
+        return div(new ConstantIntegerProperty(a), b);
     }
 
     public static IntegerBinding sum(ObservableIntegerValue... values) {
@@ -85,6 +122,32 @@ public final class Bindings {
         };
     }
 
+    public static IntegerBinding min(int max, ObservableIntegerValue... values) {
+        return new IntegerBinding(values) {
+            @Override
+            protected int compute() {
+                int min = max;
+                for (ObservableIntegerValue value : values) {
+                    min = Math.min(min, value.get());
+                }
+                return min;
+            }
+        };
+    }
+
+    public static IntegerBinding max(int min, ObservableIntegerValue... values) {
+        return new IntegerBinding(values) {
+            @Override
+            protected int compute() {
+                int max = min;
+                for (ObservableIntegerValue value : values) {
+                    max = Math.max(max, value.get());
+                }
+                return max;
+            }
+        };
+    }
+
     public static FloatBinding add(ObservableFloatValue a, ObservableFloatValue b) {
         return new FloatBinding(a, b) {
             @Override
@@ -92,6 +155,14 @@ public final class Bindings {
                 return a.get() + b.get();
             }
         };
+    }
+
+    public static FloatBinding add(ObservableFloatValue a, float b) {
+        return add(a, new ConstantFloatProperty(b));
+    }
+
+    public static FloatBinding add(float a, ObservableFloatValue b) {
+        return add(new ConstantFloatProperty(a), b);
     }
 
     public static FloatBinding sub(ObservableFloatValue a, ObservableFloatValue b) {
@@ -103,6 +174,14 @@ public final class Bindings {
         };
     }
 
+    public static FloatBinding sub(ObservableFloatValue a, float b) {
+        return sub(a, new ConstantFloatProperty(b));
+    }
+
+    public static FloatBinding sub(float a, ObservableFloatValue b) {
+        return sub(new ConstantFloatProperty(a), b);
+    }
+
     public static FloatBinding mul(ObservableFloatValue a, ObservableFloatValue b) {
         return new FloatBinding(a, b) {
             @Override
@@ -110,6 +189,14 @@ public final class Bindings {
                 return a.get() * b.get();
             }
         };
+    }
+
+    public static FloatBinding mul(ObservableFloatValue a, float b) {
+        return mul(a, new ConstantFloatProperty(b));
+    }
+
+    public static FloatBinding mul(float a, ObservableFloatValue b) {
+        return mul(new ConstantFloatProperty(a), b);
     }
 
     public static FloatBinding div(ObservableFloatValue a, ObservableFloatValue b) {
@@ -121,6 +208,14 @@ public final class Bindings {
         };
     }
 
+    public static FloatBinding div(ObservableFloatValue a, float b) {
+        return div(a, new ConstantFloatProperty(b));
+    }
+
+    public static FloatBinding div(float a, ObservableFloatValue b) {
+        return div(new ConstantFloatProperty(a), b);
+    }
+
     public static FloatBinding sum(ObservableFloatValue... values) {
         return new FloatBinding(values) {
             @Override
@@ -130,6 +225,32 @@ public final class Bindings {
                     sum += value.get();
                 }
                 return sum;
+            }
+        };
+    }
+
+    public static FloatBinding min(ObservableFloatValue... values) {
+        return new FloatBinding(values) {
+            @Override
+            protected float compute() {
+                float min = 0;
+                for (ObservableFloatValue value : values) {
+                    min = Math.min(min, value.get());
+                }
+                return min;
+            }
+        };
+    }
+
+    public static FloatBinding min(float max, ObservableFloatValue... values) {
+        return new FloatBinding(values) {
+            @Override
+            protected float compute() {
+                float min = max;
+                for (ObservableFloatValue value : values) {
+                    min = Math.min(min, value.get());
+                }
+                return min;
             }
         };
     }
@@ -147,15 +268,42 @@ public final class Bindings {
         };
     }
 
-    public static FloatBinding min(ObservableFloatValue... values) {
+    public static FloatBinding max(float min, ObservableFloatValue... values) {
         return new FloatBinding(values) {
             @Override
             protected float compute() {
-                float min = 0;
+                float max = min;
                 for (ObservableFloatValue value : values) {
-                    min = Math.min(min, value.get());
+                    max = Math.max(max, value.get());
                 }
-                return min;
+                return max;
+            }
+        };
+    }
+
+    public static <I, O> ObjectBinding<O> map(ObservableValue<I> value, Function<I, O> mapper) {
+        return new ObjectBinding<O>(value) {
+            @Override
+            protected O compute() {
+                return mapper.apply(value.getValue());
+            }
+        };
+    }
+
+    public static <T> IntegerBinding mapToInt(ObservableValue<T> value, Function<T, Integer> mapper) {
+        return new IntegerBinding(value) {
+            @Override
+            protected int compute() {
+                return mapper.apply(value.getValue());
+            }
+        };
+    }
+
+    public static <T> FloatBinding mapToFloat(ObservableValue<T> value, Function<T, Float> mapper) {
+        return new FloatBinding(value) {
+            @Override
+            protected float compute() {
+                return mapper.apply(value.getValue());
             }
         };
     }
